c620817ef93024b7486182f4c9c65f446690c432
Andreas Williams <andreas.williams12@gmail.com>
2016-09-23 13:31:41 +0100

    add format to logger

--- deepgram/log.py
+++ deepgram/log.py
@@ -1,8 +1,12 @@
 import logging
 
-FORMAT = ""
-logging.basicConfig()
+FORMAT = '[%(levelname)s]: %(asctime)s [%(pathname)s:%(lineno)s] %(message)s'
+logging.basicConfig(format=FORMAT)
 
 
 def logger():
+    """
+    Return a logger named 'deepgram' with the format set in FORMAT above.
+    """
+
     return logging.getLogger('deepgram')
