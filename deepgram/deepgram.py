3b43e9fd146c0de1ede840cfb8141c6d0100c2ab
Andreas Williams <andreas.williams12@gmail.com>
2016-09-23 18:42:52 +0100

    add function for parallel search

--- deepgram/deepgram.py
+++ deepgram/deepgram.py
@@ -157,8 +157,50 @@ class Deepgram(object):
             raise
         return response
 
-    def parallel_search(self, query):
-        pass
+    def parallel_search(self, query, **kwargs):
+        """
+        Searched in all the uploaded audio objects with the given 'tag' and
+        'term' and returns the parts of it that contain any matches to the
+        search term.
+
+        Params:
+            query  (string): The query term
+            kwargs (dict):   Extra arguments to pass to the function. Include:
+                - tag         (string): The tag to use for the search. Narrows
+                                        down the objects to be searched.
+                - snippet     (bool):   Whether to return the transcript of a
+                                        match. Default: True
+                - group_Nmax  (int):    The maximum number of objects to return
+                                        Default: 10
+                - object_Nmax (int):    The maximum number of matches to return
+                                        within the same object. Default: 10
+                - object_Pmin (float):  The minimum probability that qualifies
+                                        a match. Default: 0.55
+                - sort        (string): The term to sort by. Default: 'time'
+        """
+
+        data = {
+            "action": "parallel_search",
+            "userID": self.api_key,
+            "query": query,
+            "tag": kwargs.get("tag", ""),
+            "group_filter": {
+                "Nmax": kwargs.get("group_Nmax", 10)
+            },
+            "object_filter": {
+                "Nmax": kwargs.get("object_Nmax", 10),
+                "Pmin": kwargs.get("object_Pmin", 0.55)
+            },
+            "snippet": kwargs.get("snippet", True),
+            "sort": kwargs.get("sort", "time")
+        }
+        try:
+            response = self.group_client.post(
+                headers=self._get_headers(), params=data)
+        except Exception as e:
+            self.logger.error(e)
+            raise
+        return response
 
     def tag(self, obj, tag):
         """
