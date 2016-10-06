ba5e26627240caefd9aa35979dfbafdf5c18e940
Andreas Williams <andreas.williams12@gmail.com>
2016-10-06 18:09:21 +0100

    upgrade version to 0.1.1

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ with open('requirements.txt') as f:
     required = f.read().splitlines()
 
 setup(name='deepgram',
-      version='0.1',
+      version='0.1.1',
       description='A python wrapper for the Deepgram API',
       author='agouil',
       author_email='andreas.williams12@gmail.com',
       url='https://github.com/agouil/deepgram-python',
-      download_url='https://github.com/agouil/deepgram-python/tarball/0.1.0',
+      download_url='https://github.com/agouil/deepgram-python/tarball/0.1.1',
       packages=['deepgram'],
       keywords=['deepgram', 'audio', 'voice', 'transcription', 'api'],
       install_requires=required)
