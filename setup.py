10708c526ba419f5f08ee754b6ad07722f40181d
Andreas Williams <andreas.williams12@gmail.com>
2016-10-06 18:27:24 +0100

    update git tarball link

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='deepgram',
       author='agouil',
       author_email='andreas.williams12@gmail.com',
       url='https://github.com/agouil/deepgram-python',
-      download_url='https://github.com/agouil/deepgram-python/tarball/0.1.1',
+      download_url='https://github.com/agouil/deepgram-python/tarball/0.1.2',
       packages=['deepgram'],
       keywords=['deepgram', 'audio', 'voice', 'transcription', 'api'],
       install_requires=['requests==2.11.1'])
